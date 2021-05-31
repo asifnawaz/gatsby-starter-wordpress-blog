23abccd2d0acbfff959e2398631587313323d128
asifnawaz <iamasifnawaz@hotmail.com>
2021-06-01 02:23:17 +0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         // the only required plugin option for WordPress is the GraphQL url.
         url:
           process.env.WPGRAPHQL_URL ||
-          `https://sirfhungama.com/graphql`,
+          `https://urdunews.ga/graphql`,
       },
     },
 
